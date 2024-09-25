795775e00c6cd63d8fee49b7dd078102b2755bb0
adalberto027 <adalberto2772003@gmail.com>
2024-09-25 03:31:53 -0600

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char input[256];  // Buffer store the input of the user we can change the number of cracteres
+
+    for (;;) {
+        // Show gratter than sign
+        printf("> ");
+      
+        // read the user input
+        if (fgets(input, sizeof(input), stdin) == NULL) {
+            break;  // exit if there is no input
+        }
+
+        // remove the enter a the end of the line
+        input[strcspn(input, "\n")] = 0;
+
+        // exit ends the program
+        if (strcmp(input, "exit") == 0) {
+            break;
+        }
+
+        // return the input
+        printf("%s\n", input);
+    }
+
+    return 0;
+}
 
