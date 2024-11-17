b60e971b18d211511c7edf87deacbf2d03d9953d
adalberto027 <adalberto2772003@gmail.com>
2024-11-16 22:14:51 -0700

    Update tokenizer.c

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,45 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
 #include "tokenizer.h"
+#include "history.h"
 
 int space_char(char c) {
     return c == ' ' || c == '\t';
 }
 
-int non_space_char(char c){
-  return !space_char(c);
+int non_space_char(char c) {
+    return !space_char(c);
 }
 
-char *token_start(char *str){
-  while (*str && space_char(*str)){
-    str++;
-  }
-  return (*str) ? str : NULL;
+char *token_start(char *str) {
+    while (*str && space_char(*str)) {
+        str++;
+    }
+    return (*str) ? str : NULL;
 }
 
-char *token_terminator(char *token){
-  while (*token && non_space_char(*token)){
-    token++;
-  }
-  return token;
+char *token_terminator(char *token) {
+    while (*token && non_space_char(*token)) {
+        token++;
+    }
+    return token;
 }
 
-int count_tokens(char *str){
-  int count = 0;
-  while ((str = token_start(str))){
-    count++;
-    str = token_terminator(str);
-  }
-  return count;
+int count_tokens(char *str) {
+    int count = 0;
+    while ((str = token_start(str))) {
+        count++;
+        str = token_terminator(str);
+    }
+    return count;
 }
 
-char *copy_str(char *inStr, short len){
-  char *copy = malloc((len + 1) * sizeof(char));
-  if (!copy){
-    return NULL;
-  }
-  strncpy(copy, inStr, len);
-  copy[len] = '\0';
-  return copy;
+char *copy_str(char *inStr, short len) {
+    static char buffer[256]; // Tamaño máximo del buffer.
+    if (len >= sizeof(buffer)) {
+        len = sizeof(buffer) - 1; // Limita la longitud al tamaño del buffer.
+    }
+    strncpy(buffer, inStr, len);
+    buffer[len] = '\0';
+    return buffer;
 }
+
