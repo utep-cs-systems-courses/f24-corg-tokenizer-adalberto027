b082363c9f65fbbb51d8134ff5413461b0a57564
adalberto027 <adalberto2772003@gmail.com>
2024-11-16 20:24:04 -0700

    Update custom_tester.c

--- src/custom_tester.c
+++ src/custom_tester.c
@@ -7,10 +7,5 @@ int main() {
     printf("space_char(' ') = %d (Expected: 1)\n", space_char(' '));
     printf("space_char('a') = %d (Expected: 0)\n", space_char('a'));
 
-    // Prueba para non_space_char
-    printf("\nTesting non_space_char...\n");
-    printf("non_space_char(' ') = %d (Expected: 0)\n", non_space_char(' '));
-    printf("non_space_char('a') = %d (Expected: 1)\n", non_space_char('a'));
-
     return 0;
 }
