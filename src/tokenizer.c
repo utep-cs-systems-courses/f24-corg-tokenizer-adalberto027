d040a6c3779ace1cda6e40a59e44e669c62bcbcd
adalberto027 <adalberto2772003@gmail.com>
2024-11-16 20:52:35 -0700

    funtions

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -6,3 +6,20 @@ int space_char(char c) {
     return c == ' ' || c == '\t';
 }
 
+int non_space_char(char c){
+  return !space_char(c);
+}
+
+char *token_start(char *str){
+  while (*str && space_char(*str)){
+    str++;
+  }
+  return (*str) ? str : NULL;
+}
+
+char *token_terminator(char *token){
+  while (*token && non_space_char(*token)){
+    token++;
+  }
+  return token;
+}
